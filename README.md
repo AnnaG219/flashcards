b4697ba7faa9620d25a00efa5676abd6967be972
Anna Gibbons and Tessa Sullivan <anna@>
2019-03-26 14:22:21 -0700

    add an html page that uses jQuery to switch between a terms and its definition.

---
+++ README.md
@@ -0,0 +1,29 @@
+# _Flashcards jQuery_
+
+#### _jQuery Basics Practice Epicodus, 3.26.2019_
+
+#### By _**Tessa Sullivan, Anna Gibbons**_
+
+## Description
+
+_This web page uses jQuery functions to create flashcard buttons for all of the need to know jQuery terms. so by clicking the button we will be presented with the definition of said term, and then by clicking the definition again we will be brought back to the original term._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _find file and open in your web browser_
+
+## Known Bugs
+
+__
+
+
+## Technologies Used
+
+_jQuery, Bootstrap, CSS, HTML_
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2019 _Tessa Sullivan, Anna Gibbons__
---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#javascriptDef").show();
+    $("#javascript").hide();
+  });
+  $("#javascriptDef").click(function() {
+    $("#javascript").show();
+    $("#javascriptDef").hide();
+  });
+  $("#operators").click(function() {
+    $("#operatorsDef").show();
+    $("#operators").hide();
+  });
+  $("#operatorsDef").click(function() {
+    $("#operators").show();
+    $("#operatorsDef").hide();
+  });
+  $("#variables").click(function() {
+    $("#variablesDef").show();
+    $("#variables").hide();
+  });
+  $("#variablesDef").click(function() {
+    $("#variables").show();
+    $("#variablesDef").hide();
+  });
+  $("#functions").click(function() {
+    $("#functionsDef").show();
+    $("#functions").hide();
+  });
+  $("#functionsDef").click(function() {
+    $("#functions").show();
+    $("#functionsDef").hide();
+  });
+});
