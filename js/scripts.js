0cef137aed9be12dc6d5364381e8ae7ce41f33c2
Anna Gibbons and Tessa Sullivan <anna@>
2019-03-26 14:29:50 -0700

    Finished adding code to show/hide the definitions.

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,52 @@ $(document).ready(function() {
     $("#functions").show();
     $("#functionsDef").hide();
   });
+  $("#methods").click(function() {
+    $("#methodsDef").show();
+    $("#methods").hide();
+  });
+  $("#methodsDef").click(function() {
+    $("#methods").show();
+    $("#methodsDef").hide();
+  });
+  $("#frontEndLogic").click(function() {
+    $("#frontEndLogicDef").show();
+    $("#frontEndLogic").hide();
+  });
+  $("#frontEndLogicDef").click(function() {
+    $("#frontEndLogic").show();
+    $("#frontEndLogicDef").hide();
+  });
+  $("#backendLogic").click(function() {
+    $("#backendLogicDef").show();
+    $("#backendLogic").hide();
+  });
+  $("#backendLogicDef").click(function() {
+    $("#backendLogic").show();
+    $("#backendLogicDef").hide();
+  });
+  $("#dom").click(function() {
+    $("#domDef").show();
+    $("#dom").hide();
+  });
+  $("#domDef").click(function() {
+    $("#dom").show();
+    $("#domDef").hide();
+  });
+  $("#jQuery").click(function() {
+    $("#jQueryDef").show();
+    $("#jQuery").hide();
+  });
+  $("#jQueryDef").click(function() {
+    $("#jQuery").show();
+    $("#jQueryDef").hide();
+  });
+  $("#strings").click(function() {
+    $("#stringsDef").show();
+    $("#strings").hide();
+  });
+  $("#stringsDef").click(function() {
+    $("#strings").show();
+    $("#stringsDef").hide();
+  });
 });
